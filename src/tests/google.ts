12e4c3ebe7df3cbd3ac0f18f90db9a1eff3071d4
Priit Kallas <kallaspriit@gmail.com>
2017-08-18 09:35:37 +0300

    Now adding changed files to git.

--- src/tests/google.ts
+++ src/tests/google.ts
@@ -4,8 +4,6 @@ export default {
 	'search for nightwatch finds the github repository': (client: NightWatchClient) => {
 		client.page.google().navigate().enterQuery(client, 'nightwatch');
 
-		client
-		
-				.assert.title('nightwatch - Google Search').assert.containsText('//*[@id="main"]', 'Nightwatch.js').end();
+		client.assert.title('nightwatch - Google Search').assert.containsText('//*[@id="main"]', 'Nightwatch.js').end();
 	},
 };
